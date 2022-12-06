549ca811a1a64acac83cb44608e38914aeb6ce1f
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 17:38:39 +0300

    updated serilizers

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :product_id, :name, :rating, :comment
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :first_name, :last_name, :email, :isAdmin, :shipping, :payment
+  has_many :orders
 end
