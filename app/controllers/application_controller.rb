f3ae506211c83c7a11f2092e08f90bcb80c25380
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 17:57:35 +0300

    added seed and updated user controller to display all users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
-end
+    
+    def index
+        @users = User.all
+           if @users
+              render json: {
+              users: @users
+           }
+          else
+              render json: {
+              status: 500,
+              errors: ['no users found']
+          }
+         end
+    end
+ 
+  
+  private
+      
+     def user_params
+         params.permit(:id, :first_name, :last_name, :email, :password, :password_confirmation, :isAdmin, :shipping => {}, :payment => {})
+     end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create(first_name: 'Timothy', last_name: 'Mureithi', email: 'admin@main.com', password: 'zxcvb', password_confirmation: 'zxcvb', isAdmin: true)
+user2 = User.create(first_name: 'Judy', last_name: 'Muthoni', email: 'judy@mail.com', password: 'asdfg', password_confirmation: 'asdfg', isAdmin: false)
+user3 = User.create(first_name: 'Eric', last_name: 'Mureithi', email: 'eric@mail.com', password: 'eric23', password_confirmation: 'eric123', isAdmin: false)
