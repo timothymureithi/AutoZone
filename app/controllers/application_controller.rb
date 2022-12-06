1582e58dc6ca09fe1b8e6c9bd616be0282eaab2c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 18:25:59 +0300

    added method to create new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,4 @@
 class ApplicationController < ActionController::API
-    
-    def index
-        @users = User.all
-           if @users
-              render json: {
-              users: @users
-           }
-          else
-              render json: {
-              status: 500,
-              errors: ['no users found']
-          }
-         end
-    end
- 
+
   
-  private
-      
-     def user_params
-         params.permit(:id, :first_name, :last_name, :email, :password, :password_confirmation, :isAdmin, :shipping => {}, :payment => {})
-     end
   end
